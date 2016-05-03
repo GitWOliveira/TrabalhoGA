91175255c6782ab5b9c3fba11cdf1bcf3727a009
Gitbones <william3.npm@gmail.com>
2016-05-03 18:06:08 -0300

    Ajustado para o element.

--- src/SlideViewer/ListIthem.java
+++ src/SlideViewer/ListIthem.java
@@ -3,5 +3,28 @@ package SlideViewer;
 public class ListIthem extends Element {
     private boolean numbered;
     private int order;
+
+    public ListIthem(boolean numbered, int order, String e) {
+        super(e);
+        this.numbered = numbered;
+        this.order = order;
+    }
+    
+    public boolean isNumbered() {
+        return numbered;
+    }
+
+    public void setNumbered(boolean numbered) {
+        this.numbered = numbered;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+    
     
 }
