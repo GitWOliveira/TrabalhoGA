12c0cbe35de87f2afde1fb7cdd1e69c34f896f98
vdschuck <vinicius.schuck@live.com>
2016-05-03 00:09:34 -0300

    Criação do construtor

--- src/SlideViewer/Title.java
+++ src/SlideViewer/Title.java
@@ -1,6 +1,24 @@
 package SlideViewer;
 
 public class Title extends Element{
-  private boolean capital; 
-  
+    private boolean capital = true; 
+    private String titulo;
+    
+    public Title(String titulo){
+        if(titulo == null || "".equals(titulo.trim())){
+            capital = false;
+            print(capital);
+        }else{
+            capital = true;
+            this.titulo = titulo;
+            print(capital);
+        }
+    }
+    
+    public String print(boolean capital){        
+        if(capital == true)
+            return this.titulo;
+        else 
+            return "";
+    }
 }
