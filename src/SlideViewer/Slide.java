7a6e7dfa9c5f99970e0f916413114161326c24e2
Gitbones <william3.npm@gmail.com>
2016-05-03 03:15:16 -0300

    Gets e sets.

--- src/SlideViewer/Slide.java
+++ src/SlideViewer/Slide.java
@@ -9,6 +9,31 @@ public class Slide implements Printable {
         this.title = new Title(line);
         this.style = s;
     }
+
+    public Element getElem() {
+        return elem;
+    }
+
+    public void setElem(Element elem) {
+        this.elem = elem;
+    }
+
+    public Title getTitle() {
+        return title;
+    }
+
+    public void setTitle(Title title) {
+        this.title = title;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public void setStyle(Style style) {
+        this.style = style;
+    }
+    
     
    public void addElement(Element elem){
        this.elem = elem;       
