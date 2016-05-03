9d4a33ab6aee74e76cade30b4863574fc9e2c58a
vdschuck <vinicius.schuck@live.com>
2016-05-03 20:34:30 -0300

    criação do marcador

--- src/SlideViewer/ListIthem.java
+++ src/SlideViewer/ListIthem.java
@@ -1,8 +1,12 @@
 package SlideViewer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListIthem extends Element {
     private boolean numbered;
     private int order;
+    private char vetorLetras[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
     public ListIthem(boolean numbered, int order, String e) {
         super(e);
@@ -24,7 +28,17 @@ public class ListIthem extends Element {
 
     public void setOrder(int order) {
         this.order = order;
-    }
+    }    
     
+    public void marcadores(String line){
+        int numerador=0, i=0;
+        char letras;        
+        String[] linha = line.split(" ");
+        
+        if("#".equals(linha[0]))
+            numerador = numerador+1;
+        else if("##".equals(linha[0]))
+            letras = vetorLetras[i++]; 
+    }
     
 }
