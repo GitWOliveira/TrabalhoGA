203ea074928905d5f0547d233c30aa7a133555f5
Gitbones <william3.npm@gmail.com>
2016-05-03 17:44:32 -0300

    Ajustado o element de acordo com a UML.

--- src/SlideViewer/Element.java
+++ src/SlideViewer/Element.java
@@ -3,6 +3,19 @@ package SlideViewer;
 public class Element implements Printable {
     private String text;
    
+    public Element(String e){
+        try{
+            this.text = e;
+            print();
+        }
+        catch(NullPointerException n){
+            System.out.println("Valores inválido, digite um valor correto");
+        }
+        catch(Exception x){
+            System.out.println("Digite um valor de acordo com o padrõa");
+        }
+    }
+    
     public String print(){
         return text;    
     }
