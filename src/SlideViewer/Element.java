1202452482ac84cb497b8be5ad95d619d1974f2b
Gitbones <william3.npm@gmail.com>
2016-05-03 01:41:22 -0300

    Ajuste do método print.

--- src/SlideViewer/Element.java
+++ src/SlideViewer/Element.java
@@ -2,9 +2,9 @@ package SlideViewer;
 
 public class Element implements Printable {
     private String text;
-    
+   
     public String print(){
-        return "";    
+        return text;    
     }
     
 }
