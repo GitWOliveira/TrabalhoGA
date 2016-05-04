576d2c7c4b31e8b0d989866f3dfb98b09ea30272
Gitbones <william3.npm@gmail.com>
2016-05-04 19:48:40 -0300

    Add menu.

--- src/SlideViewer/SlideViewerApp.java
+++ src/SlideViewer/SlideViewerApp.java
@@ -5,19 +5,50 @@
  */
 package SlideViewer;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author W.Oliveira
  */
 public class SlideViewerApp {
     public static void main(String[] args){
-        try{
-            PresentationLoader pl = new PresentationLoader();
-            Presentation pp = pl.loader("presentation.txt");
-            Navigator nav = new Navigator(pp.getSlide());           
+        try {
+          PresentationLoader pl = new PresentationLoader();
+           Presentation pp = pl.loader("presentation.txt");            
+           Navigator nav = new Navigator(pp.getSlide());
+            
+            
+            int op = 0;
+            do {
+                try{
+                    String temp = JOptionPane.showInputDialog("1-Anterior, 2-Proximo, 3-Especifico, 0-Sair");
+                    op = Integer.valueOf(temp);
+                    switch (op) {
+                        case 1:
+                            nav.previous();
+                            break;
+                        case 2:
+                            nav.next();
+                            break;
+                        case 3:
+                            String num = JOptionPane.showInputDialog("Numero do slide: ");
+                            int n = Integer.valueOf(num);
+                            nav.Goto(n);
+                        case 0:
+                            break;                        
+                    }
+                    
+                }catch (NumberFormatException ex) {
+                    op = 0;
+                }catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, e);
+                    op = -1;
+                }
+            }while (op != 0);
+            
+            } catch (Exception ex) {
+                System.out.println(ex);
         }
-        catch(Exception e){
-            System.out.println(e);
-        }          
     } 
 }
