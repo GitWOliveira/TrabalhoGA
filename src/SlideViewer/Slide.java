c6c97cc9ae4a6e6c59212c969e70621070058c0c
vdschuck <vinicius.schuck@live.com>
2016-05-03 21:40:39 -0300

    alteração do print

--- src/SlideViewer/Slide.java
+++ src/SlideViewer/Slide.java
@@ -41,7 +41,17 @@ public class Slide implements Printable {
    }
    
    public String print(){
-       return "";
+       char[][] matriz = new char[20][80];
+       //Imprima slides dentro de bordas (80 colunas)
+       //O slides tem no mínimo 20 linhas
+       for(int linha=0; linha < matriz.length; linha++){
+            for(int coluna=0; coluna < matriz[0].length; coluna++){
+                if(linha == 0 || linha == 19)
+                    System.out.print("_ ");
+                if(linha == 1 && coluna == 0 || linha == 79 && coluna == 0)
+                    System.out.println("|");
+            }
+       } return print(); 
    }
    
 }
