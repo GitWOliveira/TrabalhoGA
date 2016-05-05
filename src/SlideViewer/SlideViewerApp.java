0ba41f0e450fcd7e829be3c975631b8c2770d4e5
Gitbones <william3.npm@gmail.com>
2016-05-04 22:03:02 -0300

    Add alterações.

--- src/SlideViewer/SlideViewerApp.java
+++ src/SlideViewer/SlideViewerApp.java
@@ -1,48 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SlideViewer;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author W.Oliveira
- */
 public class SlideViewerApp {
     public static void main(String[] args){
         try {
           PresentationLoader pl = new PresentationLoader();
-           Presentation pp = pl.loader("presentation.txt");            
-           Navigator nav = new Navigator(pp.getSlide());
+          Presentation pp = pl.loader("presentation.txt");            
+          Navigator nav = new Navigator(pp.getSlide());
             
             
             int op = 0;
             do {
-                try{
-                    String temp = JOptionPane.showInputDialog("1-Anterior, 2-Proximo, 3-Especifico, 0-Sair");
-                    op = Integer.valueOf(temp);
+                try{                   
+                    op = ReadData.readInt("1-Anterior, 2-Proximo, 3-Especifico, 0-Sair"); 
                     switch (op) {
                         case 1:
-                            nav.previous();
+                                nav.previous();
                             break;
                         case 2:
-                            nav.next();
+                                nav.next();
                             break;
                         case 3:
-                            String num = JOptionPane.showInputDialog("Numero do slide: ");
-                            int n = Integer.valueOf(num);
-                            nav.Goto(n);
+                            int num = ReadData.readInt("Numero do slide: ");                            
+                                nav.Goto(num);
                         case 0:
                             break;                        
-                    }
-                    
+                    }                    
                 }catch (NumberFormatException ex) {
                     op = 0;
-                }catch (Exception e) {
-                    JOptionPane.showMessageDialog(null, e);
+                }catch (NullPointerException ex) {
+                    JOptionPane.showMessageDialog(null, ex);
                     op = -1;
                 }
             }while (op != 0);
@@ -50,5 +38,6 @@ public class SlideViewerApp {
             } catch (Exception ex) {
                 System.out.println(ex);
         }
+        
     } 
 }
