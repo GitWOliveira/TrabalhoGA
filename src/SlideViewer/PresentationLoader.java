a862ed62265d26483c86b860b7cf34c58106d852
Gitbones <william3.npm@gmail.com>
2016-05-07 12:17:50 -0300

    Ajustado o loader e readStyle. Criado e coonfigurado o readFooter.

--- src/SlideViewer/PresentationLoader.java
+++ src/SlideViewer/PresentationLoader.java
@@ -16,10 +16,10 @@ public class PresentationLoader {
         in = new BufferedReader(fr);
         String line;
         
-        //Montando o slides
+        //Quantidade de slides
         Slide slides[] = new Slide[4];
-        Footer fo = null;
-        Presentation p = new Presentation(2,slides);
+        //Variavel que será montada e retornada pelo método
+        Presentation p = null;
         
         try {
             while ((line = in.readLine()) != null) {
@@ -28,38 +28,39 @@ public class PresentationLoader {
                 else if("/presentation".equals(line)){
                    break;                 
                 }
+                else if("presentation".equals(line)){
+                    p = new Presentation(2,slides);
+                }
                 //Ajustes do style
                 else if("styles".equals(line)){
-                    readStyle(line, in,p);
+                    readStyle(in,p);
                 }             
                 //Ajuste footer
-                else if(line.startsWith("left=") || line.startsWith("right")){
-                    fo = readFooter(line,fo);
-                    if(fo != null){
-                        p.setFoo(fo);
-                    }
+                else if("footer".equals(line)){
+                    readFooter(in,p);
                 }
                 //Ajuste Slide
-                else if(line.startsWith("title=") || line.startsWith("0") || line.startsWith("*")){
-                    slides = readSlide(line,slides,p);
+                else if("slide".equals(line)){
+                   readSlide(in,p);
                 }
             
             }
         } catch (FileNotFoundException e) {
             System.out.println("Arquivo \"" + filename + "\" não existe.");
         } catch (IOException e) {
-            System.out.println("Erro na leitura do arquivo " + filename + ".");
+            System.out.println("Erro na leitura do arquivo " + filename + "." + e);
         } finally {
             try {
                 in.close();
             } catch (Exception e) {
-                e.printStackTrace();
+                System.out.println("Erro ao criar a apresentação");
             }
         }
         return p;
     }
     
-    public void readStyle(String lines,BufferedReader in,Presentation p){
+    public void readStyle(BufferedReader in,Presentation p){
+        String lines;
         try{
             while ((lines = in.readLine()) != null){
         
@@ -69,90 +70,95 @@ public class PresentationLoader {
                 if(lines.startsWith("style=")){
                     lines = lines.substring(6);
                     String[] cores = lines.split(";");
-                    Style s = new Style(cores[0],cores[1],cores[2],cores[3]); 
+                    Style s = new Style(cores[1],cores[2],cores[3],cores[4]); 
                     p.addStyle(s);
                 }
             }
             } catch (IOException e) {
                 System.out.println("Erro na leitura do arquivo.");
-            } finally {
-                try {
-                    in.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+            } 
     }
 
-    public Footer readFooter(String lines,Footer f){
+    public void readFooter(BufferedReader in,Presentation p){
+        String lines;
+        Footer f = new Footer("","","");
+        
         try{
-            if(f == null){
-                f = new Footer(lines);
+            while ((lines = in.readLine()) != null){
+        
+                if("/footer".equals(lines)){
+                    break;
+                }         
+                
+                if(lines.startsWith("left=")){
+                  f.setLeft(lines.substring(5));
+                }
+                else if(lines.startsWith("center=")){
+                  f.setCenter(lines.substring(7));
+                }
+                else if(lines.startsWith("right=")){
+                  f.setRight(lines.substring(6));
+                }
             }
-            else{
-                f.setRight(lines);
-            }
-            return f;
-        }
-        catch(Exception e){
-            System.out.println("Digite um numero válido");
-            return null;
+        p.setFoo(f);
+        } catch (IOException e) {
+                System.out.println("Erro na leitura do arquivo.");
         }
     }
     
-    public Slide[] readSlide(String lines,Slide[] s,Presentation p){
+    public void readSlide(BufferedReader in,Presentation p){
+        String lines;
         
-        if(lines.startsWith("title")){
-            String titulo = lines.substring(6);
-            Title t = new Title(titulo);
-            int i;
-            for(i = 0;i < s.length;i++){
-                if(s[i].getTitle() == null){
-                   s[i].setTitle(t);
-                   break;
-                }
-            }       
-        }
-        
-        if(lines.contains("style")){
-            for(int i = 0;i < s.length;i++){
-                if(s[i].getStyle() == null){
-                    if(lines.startsWith("0"))
-                      s[i].setStyle(p.getStyle(0));
-                    else{
-                      s[i].setStyle(p.getStyle(1));
-                    }
-                }
-            }
-        }
-        
-        if(lines.contains("*")){
-            ListIthem list = new ListIthem(lines);
-            for(int i = 0;i < s.length;i++){
-                for(int j=0;j < s[i].getElem().length;j ++){
-                   if(s[i].getElem(j) == null) 
-                    s[i].addElement(list.marcadores(lines));
-                   break;
-                }
-
-            }
-        }
-        
-        else if(lines.startsWith("0") || lines.startsWith("1")){
-            for(int i = 0;i < s.length;i++){
-                if(s[i].getStyle() == null && lines.startsWith("0")){
-                    s[i].setStyle(p.getStyle(0));
-                }
-                else{
-                   s[i].setStyle(p.getStyle(1)); 
-                }
-            }
-         }
-        
-        else if(lines.startsWith("#") || lines.startsWith("##")){
-            
-        }
-        return s;
+//        if(lines.startsWith("title")){
+//            String titulo = lines.substring(6);
+//            Title t = new Title(titulo);
+//            int i;
+//            for(i = 0;i < s.length;i++){
+//                if(s[i].getTitle() == null){
+//                   s[i].setTitle(t);
+//                   break;
+//                }
+//            }       
+//        }
+//        
+//        if(lines.contains("style")){
+//            for(int i = 0;i < s.length;i++){
+//                if(s[i].getStyle() == null){
+//                    if(lines.startsWith("0"))
+//                      s[i].setStyle(p.getStyle(0));
+//                    else{
+//                      s[i].setStyle(p.getStyle(1));
+//                    }
+//                }
+//            }
+//        }
+//        
+//        if(lines.contains("*")){
+//            ListIthem list = new ListIthem(lines);
+//            for(int i = 0;i < s.length;i++){
+//                for(int j=0;j < s[i].getElem().length;j ++){
+//                   if(s[i].getElem(j) == null) 
+//                    s[i].addElement(list.marcadores(lines));
+//                   break;
+//                }
+//
+//            }
+//        }
+//        
+//        else if(lines.startsWith("0") || lines.startsWith("1")){
+//            for(int i = 0;i < s.length;i++){
+//                if(s[i].getStyle() == null && lines.startsWith("0")){
+//                    s[i].setStyle(p.getStyle(0));
+//                }
+//                else{
+//                   s[i].setStyle(p.getStyle(1)); 
+//                }
+//            }
+//         }
+//        
+//        else if(lines.startsWith("#") || lines.startsWith("##")){
+//            
+//        }
     }
 }
     
