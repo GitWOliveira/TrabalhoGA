2b92d9d984ed69715aaa12429d9daa18f4b3736e
Gitbones <william3.npm@gmail.com>
2016-05-07 09:09:19 -0300

    add estrutura da classe SlideDesigner.

---
+++ src/SlideViewer/SlideDesigner.java
@@ -0,0 +1,34 @@
+package SlideViewer;
+
+public class SlideDesigner {
+    
+    public String drawSlide(Slide slide,int page,Footer footer){
+        
+        return "";
+    }
+    
+    private String drawTitle(Title title){
+        
+        return "";
+    }
+    
+    private String drawBody(Slide slide){
+        
+        return "";
+    }
+    
+    private String drawFooter(Footer footer,int page){
+        
+        return "";
+    }
+    
+    private String getTextColor(String name){
+        
+        return "";
+    }
+    
+    private String getBackColor(String name){
+        
+        return "";
+    }
+}
