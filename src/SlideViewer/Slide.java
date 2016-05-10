a6bf49014fb62bc7152b4c8b0dd2a76ad19e699a
vdschuck <vinicius.schuck@live.com>
2016-05-09 23:01:20 -0300

    alteração do print

--- src/SlideViewer/Slide.java
+++ src/SlideViewer/Slide.java
@@ -59,17 +59,19 @@ public class Slide implements Printable {
     }
    
    public String print(){
-       char[][] matriz = new char[20][80];
-       //Imprima slides dentro de bordas (80 colunas)
-       //O slides tem no mínimo 20 linhas
-       for(int linha=0; linha < matriz.length; linha++){
-            for(int coluna=0; coluna < matriz[0].length; coluna++){
+        String[][] matriz = new String[20][80];
+        
+        for(int linha=0; linha<matriz.length; linha++){
+            for(int coluna=0; coluna<matriz[linha].length; coluna++){
+                matriz[linha][coluna] = "";
+                
                 if(linha == 0 || linha == 19)
                     System.out.print("_ ");
                 if(linha == 1 && coluna == 0 || linha == 79 && coluna == 0)
                     System.out.println("|");
-            }
-       } return print();// Não entendi essa parte; 
+            }       
+        }return print();
+       
    }
    
 }
